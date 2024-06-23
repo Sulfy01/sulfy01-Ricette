9198d7e4112b66c1cbc2db33bf8346523b7698a0
AntonioSulfaro <antonio.sulfaro@mail.polimi.it>
2024-06-23 22:16:04 +0200

    create_recipe improve

--- js/new_recipe.js
+++ js/new_recipe.js
@@ -9,9 +9,12 @@ function addIngredient() {
     const ingredientDiv = document.createElement('div');
     ingredientDiv.className = 'ingredient';
     ingredientDiv.innerHTML = `
-        <input type="text" name="ingredient-name" placeholder="Name">
-        <input type="number" name="ingredient-amount" placeholder="Amount">
-        <input type="text" name="ingredient-unit" placeholder="Unit">
+        <hr>
+        <input type="text" name="ingredient-name" placeholder="Nome">
+        <div class="ingredient-details">
+            <input type="number" name="ingredient-amount" placeholder="Quantità (se indefinita lasciare vuoto)">
+            <input type="text" name="ingredient-unit" placeholder="Unità di misura">
+        </div>
     `;
     document.getElementById('ingredients-list').appendChild(ingredientDiv);
 }
@@ -20,14 +23,14 @@ function addStep() {
     const stepDiv = document.createElement('div');
     stepDiv.className = 'procedure-step';
     stepDiv.innerHTML = `
-        <textarea name="step" placeholder="Step"></textarea>
+        <textarea name="step" placeholder="Step" rows="5"></textarea>
     `;
     document.getElementById('procedure-list').appendChild(stepDiv);
 }
 
 function generateAndDownloadJSON() {
     if (!checkRequiredFields()) {
-        alert('Please fill in all required fields.');
+        alert('Compila tutti i cambi richiesti!');
         return;
     }
 
@@ -35,7 +38,7 @@ function generateAndDownloadJSON() {
     const recipe = {
         title: formData.get('title'),
         type: formData.get('type'),
-        time: parseInt(formData.get('time')),
+        time: formData.get('time'),
         diners: [
             formData.get('diners-quantity') + ' ' + formData.get('diners-unit')
         ],
@@ -54,7 +57,8 @@ function generateAndDownloadJSON() {
     const ingredientsList = document.querySelectorAll('#ingredients-list .ingredient');
     ingredientsList.forEach((ingredient) => {
         const name = ingredient.querySelector('input[name="ingredient-name"]').value;
-        const amount = ingredient.querySelector('input[name="ingredient-amount"]').value;
+        let amount = ingredient.querySelector('input[name="ingredient-amount"]').value;
+        if (!amount) amount = "q.b."
         const unit = ingredient.querySelector('input[name="ingredient-unit"]').value;
         recipe.ingredients.push({ name, amount, unit });
     });
